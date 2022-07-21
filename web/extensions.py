6cac345363c3d34155dae7a7b1a7aa28a84e921b
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2022-07-20 21:26:23 -0500

    update admin nav

--- web/extensions.py
+++ web/extensions.py
@@ -1,7 +1,21 @@
+from flask import url_for
 from flask_admin import Admin
+from flask_admin.menu import MenuLink
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 admin = Admin()
 db = SQLAlchemy()
 migrate = Migrate()
+
+
+class PublicSiteLink(MenuLink):
+    def get_url(self):
+        return url_for("example.index")
+
+
+# Remove duplicitous "Home" link on admin menu
+admin._menu.pop()
+
+# Add link on the admin menu leading back to main site
+admin.add_link(PublicSiteLink(name="Public Site"))
