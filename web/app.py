c84d9891eaedd916dd0524d2b0400cf388f13700
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2022-07-20 20:37:19 -0500

    register extensions on app

--- web/app.py
+++ web/app.py
@@ -5,6 +5,7 @@ def create_app(config='web.settings'):
     app = Flask(__name__)
     app.config.from_object(config)
     register_blueprints(app)
+    register_extensions(app)
     return app
 
 
@@ -12,3 +13,15 @@ def register_blueprints(app):
     from web import index
 
     app.register_blueprint(index.routes)
+
+
+def register_extensions(app):
+    from web.extensions import (
+        admin,
+        db,
+        migrate
+    )
+
+    admin.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
