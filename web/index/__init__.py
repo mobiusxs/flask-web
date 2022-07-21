9d3b8d0b57fa45df683e57b161642633711c8e18
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2022-07-20 21:49:57 -0500

    add POST for form submission

--- web/index/__init__.py
+++ web/index/__init__.py
@@ -4,4 +4,4 @@ from . import views
 
 routes = Blueprint('index', __name__, template_folder='templates')
 
-routes.add_url_rule('/', endpoint='index', view_func=views.index, methods=['GET'])
+routes.add_url_rule('/', endpoint='index', view_func=views.index, methods=['GET', 'POST'])
