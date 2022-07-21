e633be0f4fc3f5de4b7f6fcda0df8bcd60e1d533
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2022-07-20 20:20:00 -0500

    create register_blueprints function; register index

--- web/app.py
+++ web/app.py
@@ -4,4 +4,11 @@ from flask import Flask
 def create_app(config='web.settings'):
     app = Flask(__name__)
     app.config.from_object(config)
+    register_blueprints(app)
     return app
+
+
+def register_blueprints(app):
+    from web import index
+
+    app.register_blueprint(index.routes)
