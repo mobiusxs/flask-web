c973b54f0f1088c39163f020cc515462bd95a313
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2022-07-20 21:50:15 -0500

    create model

---
+++ web/index/models.py
@@ -0,0 +1,14 @@
+from web.extensions import admin, db
+from flask_admin.contrib.sqla import ModelView
+
+
+class LogbookModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first = db.Column(db.String(80), unique=True, nullable=False)
+    last = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'{self.first} {self.last}'
+
+
+admin.add_view(ModelView(LogbookModel, db.session, name='Logbook'))
