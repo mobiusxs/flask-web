5cd3e99550fbac386162709c2580ded2dcec4866
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2022-07-20 22:10:31 -0500

    configure logging

--- web/app.py
+++ web/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 def create_app(config='web.settings'):
     app = Flask(__name__)
     app.config.from_object(config)
+    configure_logging(app)
     register_blueprints(app)
     register_extensions(app)
     register_shell_context(app)
@@ -44,3 +45,24 @@ def register_error_handlers(app):
 
     for code in [401, 404, 500]:
         app.register_error_handler(code, handler)
+
+
+def configure_logging(app):
+    from logging import getLogger, DEBUG, WARNING
+    from web.logging import stream_handler
+
+    app.logger.setLevel(DEBUG)              # default level is DEBUG
+    app.logger.handlers = []                # remove default handler
+    app.logger.addHandler(stream_handler)   # add your own handlers
+
+    @app.before_first_request
+    def set_werkzeug_level():
+        werkzeug = getLogger('werkzeug')
+        werkzeug.setLevel(WARNING)
+
+    @app.after_request
+    def log_request(response):
+        app.logger.info(f'{request.remote_addr} {request.method} {request.full_path} {response.status_code}')
+        return response
+
+
