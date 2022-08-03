a71f6ada110e11bbbcc1df7eab3577d9ac4c3ee7
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2022-08-02 21:00:46 -0500

    add module css

--- web/index/__init__.py
+++ web/index/__init__.py
@@ -2,6 +2,6 @@ from flask import Blueprint
 
 from . import views
 
-routes = Blueprint('index', __name__, template_folder='templates')
+routes = Blueprint('index', __name__, template_folder='templates', static_folder='static', static_url_path='/index/static')
 
 routes.add_url_rule('/', endpoint='index', view_func=views.index, methods=['GET', 'POST'])
