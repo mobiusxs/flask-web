e56c4fc8e10e27219cfcb945b6a3f536475e0130
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2022-08-02 21:06:42 -0500

    add placeholder

--- web/index/forms.py
+++ web/index/forms.py
@@ -4,5 +4,5 @@ from wtforms.validators import DataRequired
 
 
 class LogbookForm(FlaskForm):
-    first = StringField('first', validators=[DataRequired()])
-    last = StringField('last', validators=[DataRequired()])
+    first = StringField('First', validators=[DataRequired()], render_kw={"placeholder": "First"})
+    last = StringField('Last', validators=[DataRequired()], render_kw={"placeholder": "Last"})
