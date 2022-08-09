a633b8afa86f5e6194ec5711bdd0a46dcbba2b35
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2022-08-08 23:52:32 -0500

    use web static

--- web/private/__init__.py
+++ web/private/__init__.py
@@ -2,7 +2,6 @@ from flask import Blueprint
 
 from . import views
 
-routes = Blueprint('private', __name__, url_prefix='/private', template_folder='templates',
-                   static_folder='static', static_url_path='/private/static')
+routes = Blueprint('private', __name__, url_prefix='/private', template_folder='templates')
 
 routes.add_url_rule('/', endpoint='index', view_func=views.index, methods=['GET'])
--- web/public/__init__.py
+++ web/public/__init__.py
@@ -2,7 +2,6 @@ from flask import Blueprint
 
 from . import views
 
-routes = Blueprint('public', __name__, url_prefix='/', template_folder='templates',
-                   static_folder='static', static_url_path='/public/static')
+routes = Blueprint('public', __name__, url_prefix='/', template_folder='templates')
 
 routes.add_url_rule('/', endpoint='index', view_func=views.index, methods=['GET', 'POST'])
